84249b3874f0f0491a161a7905ea0cc454556adc
jmanuellh <manuellh2008@gmail.com>
2021-05-03 02:11:14 -0500

    cambiado metodo defualt por uno con composition api con ts y agregado axios

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,32 +16,68 @@
           <ion-title size="large">Inbox</ion-title>
         </ion-toolbar>
       </ion-header>
-      
+      <!-- Lista de mensajes -->
       <ion-list>
         <MessageListItem v-for="message in messages" :key="message.id" :message="message" />
       </ion-list>
+
+      <!-- Scroll de productos -->
+      <ion-list>
+        <ion-item v-for="product in products" :key="product.id">
+          <ion-label>
+            {{product.name}}
+          </ion-label>
+        </ion-item>
+      </ion-list>
+      <ion-infinite-scroll
+        @ionInfinite="getProducts()">
+      </ion-infinite-scroll>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonContent, IonHeader, IonList, IonPage, IonRefresher, IonRefresherContent, IonTitle, IonToolbar } from '@ionic/vue';
+import {
+  IonContent,
+  IonHeader,
+  IonList,
+  IonPage,
+  IonRefresher,
+  IonRefresherContent,
+  IonTitle,
+  IonToolbar,
+  IonInfiniteScroll,
+  IonLabel,
+  IonItem
+} from '@ionic/vue';
 import MessageListItem from '@/components/MessageListItem.vue';
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 import { getMessages } from '@/data/messages';
+import axios from 'axios';
 
 export default defineComponent({
   name: 'Home',
-  data() {
-    return {
-      messages: getMessages()
-    }
-  },
-  methods: {
-    refresh: (ev: CustomEvent) => {
+  setup() {
+    const messages = getMessages();
+    const refresh = (ev: CustomEvent) => {
       setTimeout(() => {
         ev.detail.complete();
       }, 3000);
+    }
+
+    const products = ref([]);
+    const getProducts = () => {
+      axios.get("https://localhost:5001/api/products").then(r => {
+        products.value = r.data
+      })
+      return []
+    }
+
+    return {
+      messages,
+      refresh,
+      products,
+      getProducts,
     }
   },
   components: {
@@ -53,7 +89,10 @@ export default defineComponent({
     IonRefresherContent,
     IonTitle,
     IonToolbar,
-    MessageListItem
-  },
-});
+    MessageListItem,
+    IonInfiniteScroll,
+    IonLabel,
+    IonItem
+  }
+})
 </script>
